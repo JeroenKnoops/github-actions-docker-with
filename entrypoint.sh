4d016a173dc28651eb06a8df4702e8b71ca44379
Jeroen Knoops <jeroen.knoops@philips.com>
2021-12-23 16:51:21 +0100

    more args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,12 @@ echo "argument0           : $0"
 echo "argument1           : $1"
 echo "argument2           : $2"
 echo "Additional arguments: $3"
+
+echo "=========================================="
+
+echo "loop through args"
+for var in "$@"
+do
+    echo "$var"
+done
+echo "=========================================="
